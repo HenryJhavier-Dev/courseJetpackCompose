974499c64a852f558bf8b892216411691f2ab640
henryjhavier <>
2023-06-20 13:45:22 +0200

    Finalizado 1Modulo de login con compose

--- app/src/main/java/com/henryjhavierdev/jeckpackcomposeinsta/login/ui/bodyScreen.kt
+++ app/src/main/java/com/henryjhavierdev/jeckpackcomposeinsta/login/ui/bodyScreen.kt
@@ -1,5 +1,6 @@
 package com.henryjhavierdev.jeckpackcomposeinsta.login.ui
 
+import android.util.Patterns
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -9,11 +10,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,6 +35,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.henryjhavierdev.jeckpackcomposeinsta.R
@@ -37,8 +51,14 @@ fun LoginBody(modifier: Modifier) {
 
     Column(modifier = modifier) {
         ImageLogo(Modifier.align(Alignment.CenterHorizontally))
-        LoginEmail(email, Modifier.padding(top = 16.dp)) { email = it }
-        LoginPass(pass, Modifier.padding(top = 16.dp)) { pass = it }
+        LoginEmail(email, Modifier.padding(top = 16.dp)) {
+            email = it
+            isLoginEnable = enableLogin(email, pass)
+        }
+        LoginPass(pass, Modifier.padding(top = 16.dp)) {
+            pass = it
+            isLoginEnable = enableLogin(email, pass)
+        }
         ForgotPassword(
             Modifier
                 .align(Alignment.End)
@@ -61,7 +81,17 @@ fun ImageLogo(modifier: Modifier) {
 
 @Composable
 fun LoginButton(isLoginEnable: Boolean, modifier: Modifier) {
-    Button(onClick = {}, enabled = isLoginEnable, modifier = modifier) {
+    Button(
+        onClick = {},
+        enabled = isLoginEnable,
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Blue,
+            disabledContainerColor = Color(0xFF78C8F9),
+            disabledContentColor = Color.White,
+            contentColor = Color.White,
+        )
+    ) {
         Text(text = "Log In")
     }
 }
@@ -96,17 +126,59 @@ fun LoginEmail(email: String, modifier: Modifier, onTextChange: (String) -> Unit
     TextField(
         value = email, onValueChange = onTextChange,
         modifier = modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Email...") },
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFFB2B2B2),
+            containerColor =  Color(0xFFFAFAFA),
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent
+        )
     )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginPass(email: String, modifier: Modifier, onTextChange: (String) -> Unit) {
+    var passwordVisibility by rememberSaveable {
+        mutableStateOf(false)
+    }
+
     TextField(
         modifier = modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Pass..") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFFB2B2B2),
+            containerColor =  Color(0xFFFAFAFA),
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent
+        ),
+        maxLines = 1,
+        onValueChange = { onTextChange(it) },
+        singleLine = true,
         value = email,
-        onValueChange = onTextChange
+        trailingIcon = {
+            val image = if (passwordVisibility) {
+                Icons.Filled.Visibility
+            } else {
+                Icons.Filled.VisibilityOff
+            }
+
+            IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                Icon(imageVector = image, contentDescription = "Icon Password")
+            }
+
+        },
+        visualTransformation = if (passwordVisibility) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        }
+
     )
+
 }
 
 @Composable
@@ -150,5 +222,10 @@ fun LoginDivider() {
         )
     }
 
+
 }
 
+fun enableLogin(pEmail: String, pPassword: String): Boolean{
+    return Patterns.EMAIL_ADDRESS.matcher(pEmail).matches() &&
+            pPassword.length > 6
+}
